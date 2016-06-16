fed360dd42b9387d84a7981df4ba289832597d6d
Ben Skeggs <bskeggs@redhat.com>
2016-06-16 17:43:15 +1000

    mmu/gp100: fork from gf100
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drm/nouveau/include/nvkm/subdev/mmu.h
+++ drm/nouveau/include/nvkm/subdev/mmu.h
@@ -68,4 +68,5 @@ int nv44_mmu_new(struct nvkm_device *, int, struct nvkm_mmu **);
 int nv50_mmu_new(struct nvkm_device *, int, struct nvkm_mmu **);
 int g84_mmu_new(struct nvkm_device *, int, struct nvkm_mmu **);
 int gf100_mmu_new(struct nvkm_device *, int, struct nvkm_mmu **);
+int gp100_mmu_new(struct nvkm_device *, int, struct nvkm_mmu **);
 #endif
