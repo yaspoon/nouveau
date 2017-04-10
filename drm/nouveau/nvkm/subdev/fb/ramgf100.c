e6c02c8d623a0bfd45eac0925a228ae9ca8f0455
Ben Skeggs <bskeggs@redhat.com>
2017-04-10 17:03:06 +1000

    fb/ram/gf10x: timing_10_15_0f
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drm/nouveau/nvkm/subdev/fb/ramgf100.c
+++ drm/nouveau/nvkm/subdev/fb/ramgf100.c
@@ -221,6 +221,7 @@ gf100_ram_calc_xits(struct gf100_ram *ram,
 		      (next->bios.timing_10_WTR & 0x7f) << 8);
 	ram_mask(fuc, 0x10f29c, 0x0000001f,
 		      (next->bios.timing_10_14 & 0xff) << 9 |
+		      (next->bios.timing_10_15 & 0x0f) << 5 |
 		      (next->bios.timing_10_0d & 0x1f));
 	ram_mask(fuc, 0x10f2a0, 0x001f8000,
 		      (next->bios.timing_10_RRD & 0x3f) << 15);
